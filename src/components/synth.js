a515d1ce2bbda5def4961b89283d88edb47af018
bend <bend71@pm.me>
2020-11-07 14:36:44 -0600

    map three notes (c3, c4 and c5) to three different buttons to be triggered on click

--- src/components/synth.js
+++ src/components/synth.js
@@ -3,19 +3,21 @@ import * as Tone from 'tone';
 
 function Synthesizer() {
 
-    function note(event) {
+    let noteArray = ["c3", "c4", "c5"];
+
+    function note(event, note) {
         event.preventDefault();
         //create a synth and connect it to the main output (your speakers)
         const synth = new Tone.Synth().toDestination();
 
         //play a middle 'C' for the duration of an 8th note
-        synth.triggerAttackRelease("C4", "8n");
+        synth.triggerAttackRelease(note , "8n");
 
     }
 
     return (
         <>
-            <button onClick={event => note(event)}>PUSH</button>
+            {noteArray.map(notes => <button onClick={event => note(event, notes)}>PUSH</button>)}
         </>
     )
 }
