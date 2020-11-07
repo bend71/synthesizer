2b37c2741add671bddb6570eee45b79a11c411ab
bend <bend71@pm.me>
2020-11-07 15:14:11 -0600

    map note and length to synth from allNote array

--- src/components/synth.js
+++ src/components/synth.js
@@ -3,21 +3,23 @@ import * as Tone from 'tone';
 
 function Synthesizer() {
 
-    let noteArray = ["c3", "c4", "c5"];
+    let allNote = [["c3", "4n"], ["c4", "8n"], ["c5", "16n"]]
 
-    function note(event, note) {
+    function note(event, note, length) {
         event.preventDefault();
         //create a synth and connect it to the main output (your speakers)
         const synth = new Tone.Synth().toDestination();
 
         //play a middle 'C' for the duration of an 8th note
-        synth.triggerAttackRelease(note , "4n");
+        synth.triggerAttackRelease(note, length);
 
     }
 
     return (
         <>
-            {noteArray.map((notes, index) => <button key={index} onClick={event => note(event, notes)}>{notes}</button>)}
+            {allNote.map((notes, index) => 
+            <button key={index} onClick={event => note(event, notes[0], notes[1])}>{notes[0]}</button>
+            )}
         </>
     )
 }
